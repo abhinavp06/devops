b92a40996cd65b4128de0203d8e2c87136543b40
Abhinav Parashar <abhinav.parashar@bfhl.in>
2023-10-26 12:39:33 +0530

    updated bash scripts

--- initialize.bash
+++ initialize.bash
@@ -1,34 +1,46 @@
 #!/bin/bash
-echo "Adding helm charts for kube-prometheus-stack"
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}Adding helm charts for kube-prometheus-stack${normal}"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+echo "${bold}------------------------------------------------------${normal}"
 
-echo "Starting minikube"
+echo "${bold}Starting minikube${normal}"
 minikube start
-echo "Adding minikube metrics-server addon"
+echo "${bold}------------------------------------------------------${normal}"
+echo "${bold}Adding minikube metrics-server addon${normal}"
 minikube addons enable metrics-server
-
-echo "Creating 'personal' and 'monitoring' namespace"
+echo "${bold}------------------------------------------------------${normal}"
+echo "${bold}Creating 'personal' and 'monitoring' namespace${normal}"
 kubectl create namespace personal
 kubectl create namespace monitoring
+echo "${bold}------------------------------------------------------${normal}"
 
-echo "Build docker image for service-a and load it in minikube"
+echo "${bold}Building docker image for service-a and loading it in minikube${normal}"
 cd service-a
 npm i
 docker build -t service-a .
 minikube image load service-a
 cd ..
-echo "Build docker image for service-b and load it in minikube"
+echo "${bold}------------------------------------------------------${normal}"
+echo "${bold}Building docker image for service-b and loading it in minikube${normal}"
 cd service-b
 npm i
 docker build -t service-b .
 minikube image load service-b
 cd ..
+echo "${bold}------------------------------------------------------${normal}"
 
-echo "Creating dryrun file for 'service-a'"
+
+echo "${bold}Creating dryrun file for 'service-a'${normal}"
 helm template dryrun ./service-a/deployment/helm-chart/ -f ./service-a/deployment/helm-chart/values.yaml > dryrun-a.yaml
-echo "Creating dryrun file for 'service-b'"
+echo "${bold}------------------------------------------------------${normal}"
+echo "${bold}Creating dryrun file for 'service-b'${normal}"
 helm template dryrun ./service-b/deployment/helm-chart/ -f ./service-b/deployment/helm-chart/values.yaml > dryrun-b.yaml
-echo "Add service-a and service-b to the cluster"
+echo "${bold}------------------------------------------------------${normal}"
+echo "${bold}Adding service-a and service-b to the cluster${normal}"
 kubectl apply -f dryrun-a.yaml -n personal
 kubectl apply -f dryrun-b.yaml -n personal
+echo "${bold}------------------------------------------------------${normal}"
 
