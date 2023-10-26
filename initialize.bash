74b6b3a944c5e88be53c2a866ae681fe3e37cc49
Abhinav Parashar <abhinav.parashar@bfhl.in>
2023-10-26 12:53:56 +0530

    updated bash scripts

--- initialize.bash
+++ initialize.bash
@@ -17,21 +17,22 @@ kubectl create namespace personal
 kubectl create namespace monitoring
 echo "${bold}------------------------------------------------------${normal}"
 
-echo "${bold}Building docker image for service-a and loading it in minikube${normal}"
+echo "${bold}Building docker image for service-a${normal}"
 cd service-a
 npm i
 docker build -t service-a .
-minikube image load service-a
 cd ..
 echo "${bold}------------------------------------------------------${normal}"
-echo "${bold}Building docker image for service-b and loading it in minikube${normal}"
+echo "${bold}Building docker image for service-b${normal}"
 cd service-b
 npm i
 docker build -t service-b .
-minikube image load service-b
 cd ..
 echo "${bold}------------------------------------------------------${normal}"
-
+echo "${bold}Loading docker images for service-a and service-b in minikube${normal}"
+minikube image load service-a
+minikube image load service-b
+echo "${bold}------------------------------------------------------${normal}"
 
 echo "${bold}Creating dryrun file for 'service-a'${normal}"
 helm template dryrun ./service-a/deployment/helm-chart/ -f ./service-a/deployment/helm-chart/values.yaml > dryrun-a.yaml
@@ -43,4 +44,3 @@ echo "${bold}Adding service-a and service-b to the cluster${normal}"
 kubectl apply -f dryrun-a.yaml -n personal
 kubectl apply -f dryrun-b.yaml -n personal
 echo "${bold}------------------------------------------------------${normal}"
-
--- reset.bash
+++ reset.bash
@@ -6,6 +6,8 @@ echo "${bold}Deleting 'personal' namespace${normal}"
 kubectl delete namespace personal
 echo "${bold}Deleting 'monitoring' namespace${normal}"
 kubectl delete namespace monitoring
+echo "${bold}Stopping minikube cluster${normal}"
+minikube stop
 echo "${bold}Deleting minikube cluster${normal}"
 minikube delete
 echo ${bold}"Deleting minikube profile${normal}"
