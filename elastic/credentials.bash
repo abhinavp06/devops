cf72ad3339f1d6eb5b1423fdb1e4378dfecca851
Abhinav Parashar <abhinav.parashar@bfhl.in>
2023-10-26 16:18:32 +0530

    added bash script to get elastic credentials

---
+++ elastic/credentials.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}ELASTIC USERNAME:${normal} $(kubectl get secret elasticsearch-master-credentials -o jsonpath='{.data}' -n elastic | jq -r .username | base64 --decode)"
+echo "${bold}ELASTIC PASSWORD:${normal} $(kubectl get secret elasticsearch-master-credentials -o jsonpath='{.data}' -n elastic | jq -r .password | base64 --decode)"
+echo "${bold}Please expose elasticsearch and kibana to access the ELK stack${normal}"
+echo "${bold}------------------------------------------------------${normal}"
