c32eb4ec0fbee3a9ba4d034209d957a44b1b4945
Abhinav Parashar <abhinav.parashar@bfhl.in>
2023-10-30 12:52:49 +0530

    updated reset script

--- reset.bash
+++ reset.bash
@@ -2,12 +2,6 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo "${bold}Deleting 'personal' namespace${normal}"
-kubectl delete namespace personal
-echo "${bold}Deleting 'monitoring' namespace${normal}"
-kubectl delete namespace monitoring
-echo "${bold}Deleting 'elastic' namespace${normal}"
-kubectl delete namespace elastic
 echo "${bold}Stopping minikube cluster${normal}"
 minikube stop
 echo ${bold}"Deleting minikube profile${normal}"
